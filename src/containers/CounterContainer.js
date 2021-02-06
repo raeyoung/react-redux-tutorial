29f5cd620f7d0d610bde7c98ecc8b4228b1380ac
raeyoung <raeyoung233@gmail.com>
2021-02-06 23:23:20 +0900

    Create CounterContainer
    Components linked to the Redux store

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter'
+
+const COunterContainer = () => {
+  return <Counter />
+}
+
+export default COunterContainer;
